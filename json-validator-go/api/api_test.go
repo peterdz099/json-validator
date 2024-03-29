30e7b985adb43dfd301aa09cb0572ccbc7f246d5
peterdz099 <piotrdziula@gmail.com>
2024-03-29 21:33:11 +0100

    Add testing files and fix TestNotValidJsons

--- json-validator-go/api/api_test.go
+++ json-validator-go/api/api_test.go
@@ -26,8 +26,8 @@ func TestValidJsons(t *testing.T) {
 	}
 
 	for _, file := range files {
-
 		filePath := filepath.Join(folderPath, file.Name())
+		fmt.Println(filePath)
 		fileContent, err := os.ReadFile(filePath)
 		if err != nil {
 			t.Fatalf("Failed to read file: %v", err)
@@ -77,7 +77,6 @@ func TestValidJsons(t *testing.T) {
 	}
 }
 
-// test to be finished
 func TestNotValidJsons(t *testing.T) {
 	router := gin.New()
 	router.POST("/validate", validate)
@@ -127,69 +126,7 @@ func TestNotValidJsons(t *testing.T) {
 
 		message := fmt.Sprintf("JSON from file %s is NOT VALID", filename)
 		expectedData := Response{
-			Valid:   true,
-			Message: message,
-		}
-
-		expectedJSON, err := json.Marshal(expectedData)
-		if err != nil {
-			t.Fatalf("Failed to marshal expected data to JSON: %v", err)
-		}
-
-		assert.Equal(t, string(expectedJSON), response.Body.String(), "Expected JSON data")
-	}
-}
-
-func TestInvalidJsonFormat(t *testing.T) {
-	router := gin.New()
-	router.POST("/validate", validate)
-
-	folderPath := "../test-files/invalid-format-jsons"
-	files, err := os.ReadDir(folderPath)
-	if err != nil {
-		t.Fatalf("Failed to read directory: %v", err)
-	}
-
-	for _, file := range files {
-
-		filePath := filepath.Join(folderPath, file.Name())
-		fileContent, err := os.ReadFile(filePath)
-		if err != nil {
-			t.Fatalf("Failed to read file: %v", err)
-		}
-
-		var requestBody bytes.Buffer
-		multipartWriter := multipart.NewWriter(&requestBody)
-
-		fileWriter, err := multipartWriter.CreateFormFile("file", filepath.Base(filePath))
-		if err != nil {
-			t.Fatalf("Failed to create form file: %v", err)
-		}
-		if _, err := fileWriter.Write(fileContent); err != nil {
-			t.Fatalf("Failed to write file content: %v", err)
-		}
-
-		if err := multipartWriter.Close(); err != nil {
-			t.Fatalf("Failed to close multipart writer: %v", err)
-		}
-
-		request, err := http.NewRequest("POST", "/validate", &requestBody)
-		if err != nil {
-			t.Fatalf("Failed to create request: %v", err)
-		}
-
-		request.Header.Set("Content-Type", multipartWriter.FormDataContentType())
-
-		filename := filepath.Base(filePath)
-		response := httptest.NewRecorder()
-
-		router.ServeHTTP(response, request)
-
-		assert.Equal(t, http.StatusOK, response.Code, "Expected HTTP status 200")
-
-		message := fmt.Sprintf("JSON from file %s is VALID", filename)
-		expectedData := Response{
-			Valid:   true,
+			Valid:   false,
 			Message: message,
 		}
 
@@ -302,4 +239,66 @@ func TestNotFound(t *testing.T) {
 	router.ServeHTTP(response, request)
 
 	assert.Equal(t, http.StatusNotFound, response.Code, "Expected HTTP status 404")
+}
+
+func TestInvalidJsonFormat(t *testing.T) {
+	router := gin.New()
+	router.POST("/validate", validate)
+
+	folderPath := "../test-files/invalid-format-jsons"
+	files, err := os.ReadDir(folderPath)
+	if err != nil {
+		t.Fatalf("Failed to read directory: %v", err)
+	}
+
+	for _, file := range files {
+
+		filePath := filepath.Join(folderPath, file.Name())
+		fileContent, err := os.ReadFile(filePath)
+		if err != nil {
+			t.Fatalf("Failed to read file: %v", err)
+		}
+
+		var requestBody bytes.Buffer
+		multipartWriter := multipart.NewWriter(&requestBody)
+
+		fileWriter, err := multipartWriter.CreateFormFile("file", filepath.Base(filePath))
+		if err != nil {
+			t.Fatalf("Failed to create form file: %v", err)
+		}
+		if _, err := fileWriter.Write(fileContent); err != nil {
+			t.Fatalf("Failed to write file content: %v", err)
+		}
+
+		if err := multipartWriter.Close(); err != nil {
+			t.Fatalf("Failed to close multipart writer: %v", err)
+		}
+
+		request, err := http.NewRequest("POST", "/validate", &requestBody)
+		if err != nil {
+			t.Fatalf("Failed to create request: %v", err)
+		}
+
+		request.Header.Set("Content-Type", multipartWriter.FormDataContentType())
+
+		filename := filepath.Base(filePath)
+		response := httptest.NewRecorder()
+
+		router.ServeHTTP(response, request)
+
+		assert.Equal(t, http.StatusOK, response.Code, "Expected HTTP status 200")
+
+		message := fmt.Sprintf("JSON from file %s is VALID", filename)
+		expectedData := Response{
+			Valid:   true,
+			Message: message,
+		}
+
+		expectedJSON, err := json.Marshal(expectedData)
+		if err != nil {
+			t.Fatalf("Failed to marshal expected data to JSON: %v", err)
+		}
+
+		assert.Equal(t, string(expectedJSON), response.Body.String(), "Expected JSON data")
+	}
 }
